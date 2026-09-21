9112658a8b18abe805e8029862aa72143b05746f
cai2761m <3425489461@qq.com>
2026-09-22 00:56:26 +0800

    init

---
+++ bin/d_t_o_t_px.dart
@@ -0,0 +1,5 @@
+import 'package:d_t_o_t_px/d_t_o_t_px.dart' as d_t_o_t_px;
+
+void main(List<String> arguments) {
+  print('Hello world: ${d_t_o_t_px.calculate()}!');
+}
---
+++ test/d_t_o_t_px_test.dart
@@ -0,0 +1,8 @@
+import 'package:d_t_o_t_px/d_t_o_t_px.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
